2e69d5b480208f86e7ffbae40510b16665ed5207
hanyu <360548055@qq.com>
2024-01-01 14:12:05 +0800

    lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -8,17 +8,25 @@
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _unassemble_map()
+    , _next_assembled_idx(0)
+    , _unassembled_bytes_num(0)
+    , _eof_idx(-1)
+    , _output(capacity)
+    , _capacity(capacity) {}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (index <= _next_assembled_idx && index + data.length() > _next_assembled_idx) {
+    } else if (index > _next_assembled_idx) {
+    }
 }
 
 size_t StreamReassembler::unassembled_bytes() const { return {}; }
